d5c17cefd46c6eb62d6b27ad949d3a8708c74f72
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-02 09:59:50 +0000

    Imported django-heroku into settings.py

--- sign_up_app_project/sign_up_app/settings.py
+++ sign_up_app_project/sign_up_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
 
@@ -121,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
